69284f2231e2637ea500655b53d75d857bb0f05a
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2023-07-27 21:34:11 +0530

    send sms in email id

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -7,6 +7,11 @@ import {
 } from "react-icons/ai";
 
 const Contact = () => {
+    const refreshPage = (e) => {
+        e.target.name.name = "";
+        e.target.name.email = "";
+        e.target.name.message = "";
+    };
     return (
         <>
             <div className="contact-main" id="contact">
@@ -16,23 +21,37 @@ const Contact = () => {
                         <div>
                             <img
                                 src="https://www.achintya.co.in/app_themes/images/contact-img.svg"
-                                alt=""
+                                alt="img"
                             />
                         </div>
                     </div>
                     <div className="col-md-6 col-sm-12 conatact-form">
                         <div className="contact-form-icons">
-                            <h4>contact with</h4> <a href="https://www.linkedin.com/in/ankit-rahi-563752258/"><AiOutlineLinkedin size={40} color="blue" /></a> {" "}
-                            <a href="https://github.com/Ankit9179?tab=repositories"><AiFillGithub size={40} /></a>
-                            <a href="https://www.instagram.com/ak_vampire12/"><AiOutlineInstagram size={40} color="red" /></a>
+                            <h4>contact with</h4>{" "}
+                            <a href="https://www.linkedin.com/in/ankit-rahi-563752258/">
+                                <AiOutlineLinkedin size={40} color="blue" />
+                            </a>{" "}
+                            <a href="https://github.com/Ankit9179?tab=repositories">
+                                <AiFillGithub size={40} />
+                            </a>
+                            <a href="https://www.instagram.com/ak_vampire12/">
+                                <AiOutlineInstagram size={40} color="red" />
+                            </a>
                             <br />
                             <h5 className="text-center">OR</h5>
                             <hr />
                         </div>
-                        <form className="form">
+                        <form
+                            onSubmit={refreshPage}
+                            className="form"
+                            action="https://formspree.io/f/xzbldppp"
+                            method="POST"
+                        >
                             <div className="mb-3">
                                 <input
-                                    type="email"
+                                    required
+                                    type="text"
+                                    name="name"
                                     className="form-control"
                                     placeholder="Enter your name"
                                     aria-describedby="emailHelp"
@@ -40,7 +59,9 @@ const Contact = () => {
                             </div>
                             <div className="mb-3">
                                 <input
+                                    required
                                     type="email"
+                                    name="email"
                                     className="form-control"
                                     placeholder="Enter your email"
                                     aria-describedby="emailHelp"
@@ -48,6 +69,9 @@ const Contact = () => {
                             </div>
                             <div className="mb-3">
                                 <textarea
+                                    type="text"
+                                    name="message"
+                                    required
                                     cols="48"
                                     rows="3"
                                     placeholder=" wright something"
@@ -55,7 +79,7 @@ const Contact = () => {
                             </div>
 
                             <button type="submit" className="btn btn-primary button-submit">
-                                Submit
+                                SUBMIT
                             </button>
                         </form>
                     </div>
