68e4a99f38689ceb7cc81f1b2dbfb82fc6cc4d85
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2023-09-14 18:33:49 +0530

    add new card

--- src/pages/projcts/Projects.js
+++ src/pages/projcts/Projects.js
@@ -20,7 +20,53 @@ const Projects = () => {
         <div className=" mb-4 mt-4 project-card-div  ">
           <div className="row" d-flex>
             <Spin>
-              <div className="col-md-4 project-card ">
+              <div className="col-md-4 project-card mt-4  ">
+                <div className="card card-div" style={{ width: "18rem" }}>
+                  <span>Full Stack</span>
+                  <img
+                    src="https://fireapps.io/wp-content/uploads/2020/08/shopping-e-commerce-concept-isometric-poster_1284-15256.jpg"
+                    alt="pic"
+                  />
+                  <div className="card-body d-flex project-use-skills">
+                    <p>MongoDB</p>
+                    <p>Express</p>
+                    <p>React</p>
+                    <p>NodeJs</p>
+                  </div>
+                  <div className="text-center">
+                    <h3>E-commerce</h3>
+                  </div>
+                  <div className="project-view-div">
+                    <a href="https://e-commerce-mern-client.onrender.com">
+                      View
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 project-card mt-4  ">
+                <div className="card card-div" style={{ width: "18rem" }}>
+                  <span>Full Stack</span>
+                  <img
+                    src="https://cdn.dribbble.com/userupload/3893934/file/original-c91e5bd66978749ff418b53442ce382b.png?resize=400x0"
+                    alt="pic"
+                  />
+                  <div className="card-body d-flex project-use-skills">
+                    <p>MongoDB</p>
+                    <p>Express</p>
+                    <p>React</p>
+                    <p>NodeJs</p>
+                  </div>
+                  <div className="text-center">
+                    <h3>Portfolio</h3>
+                  </div>
+                  <div className="project-view-div">
+                    <a href="https://portfolio-client-4air.onrender.com">
+                      View
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 project-card mt-4 ">
                 <div className="card card-div" style={{ width: "18rem" }}>
                   <span>Full Stack</span>
                   <img
@@ -42,8 +88,7 @@ const Projects = () => {
                   </div>
                 </div>
               </div>
-
-              <div className="col-md-4 project-card ">
+              <div className="col-md-4 project-card mt-4 ">
                 <div className="card card-div" style={{ width: "18rem" }}>
                   <span>Full Stack</span>
                   <img
@@ -61,30 +106,6 @@ const Projects = () => {
                   </div>
                   <div className="project-view-div">
                     <a href="https://tiny-fedora-moth.cyclic.app/login">View</a>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-md-4 project-card ">
-                <div className="card card-div" style={{ width: "18rem" }}>
-                  <span>Full Stack</span>
-                  <img
-                    src="https://cdn.dribbble.com/userupload/3893934/file/original-c91e5bd66978749ff418b53442ce382b.png?resize=400x0"
-                    alt="pic"
-                  />
-                  <div className="card-body d-flex project-use-skills">
-                    <p>MongoDB</p>
-                    <p>Express</p>
-                    <p>React</p>
-                    <p>NodeJs</p>
-                  </div>
-                  <div className="text-center">
-                    <h3>Portfolio</h3>
-                  </div>
-                  <div className="project-view-div">
-                    <a href="https://github.com/Ankit9179?tab=repositories">
-                      View
-                    </a>
                   </div>
                 </div>
               </div>
